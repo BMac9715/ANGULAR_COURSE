b1c4e1ccb8d3e70cfb894ef157f2be8c22ac5f6b
Bryan Macario Coronado <BMac9715@users.noreply.github.com>
2022-06-24 21:24:09 -0600

    Template TForest
    Template TForest

---
+++ template-tforest/src/app/catalogs/distribuitors/distribuitors.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-distribuitors',
+  templateUrl: './distribuitors.component.html',
+  styleUrls: ['./distribuitors.component.scss']
+})
+export class DistribuitorsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
